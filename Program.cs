50cce2f47d9872e95b9744a56b787a7d70856076
loftwals <70249978+loftwals@users.noreply.github.com>
2023-07-13 11:00:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejercicio 1");
+
+Console.WriteLine("Nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Apellido");
+string apellido = Console.ReadLine();
+
+
+Console.WriteLine("Edad");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sabe Programar");
+string programa = Console.ReadLine();
+
+Console.WriteLine("Hola, " + nombre + apellido + " Tenes, " + edad + " años " + "y sabes programar" + programa);
+
+
+
+Console.WriteLine("Ejercicio 2");
+string coche = "ferrari";
+int ruedas = 4;
+string marca = "ferrari";
+
+string mesa = "Comedor";
+float peso = 23.124f;
+string material = "madera";
+string color = "Madera";
+
+
+Console.WriteLine("Ejercicio 3");
+int a = 15;
+char b = 'a';
+
+Console.WriteLine("false" + (a >= 18));
+Console.WriteLine("true" + (a <= 18));
+
+
+
+
+
+
+
